9030ecc33aac671f6fa8d2f9ae3aa6f09592412d
sangaryousmane <sangaryousmane@gmail.com>
2024-03-16 06:29:43 +0000

    Complex types - mixed list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,7 +6,6 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """ Sum complex types - list of floats"""
     sum_ = 0.0
-    
     for i in input_list:
         sum_ += i
     return sum_
