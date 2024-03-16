0c1196d68aa6246b16213ab34e99c1ecb7305abc
sangaryousmane <sangaryousmane@gmail.com>
2024-03-16 06:12:58 +0000

    input_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Sum complex types - list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Sum complex types - list of floats"""
+    return sum(input_list)
