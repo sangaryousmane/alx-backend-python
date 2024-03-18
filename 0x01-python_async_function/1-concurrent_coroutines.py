55e5ba800f28b79c8430022be6b09de5a1c3fc16
sangaryousmane <sangaryousmane@gmail.com>
2024-03-18 17:33:09 +0000

    The basics of async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,9 +7,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ execute multiple coroutines at the same time with async"""
 
-    result: List[float] = []
-    i: int = 0
-    while i < n:
-        result.append(await wait_random(max_delay))
-        i += 1
-    return result
+    result: List[float] = [wait_random(max_delay) for _ in range(n)]
+    delays = asyncio.gather(*result)
+    return sorted(delays)
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 """ Measure the runtime of the coroutine"""
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
+async def measure_time(n: int, max_delay: int) -> float:
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start_time
 
+    return total_time / n
