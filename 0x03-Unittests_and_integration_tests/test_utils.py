9d91fb170ea7366765e034688c83a64d2443100e
sangaryousmane <sangaryousmane@gmail.com>
2024-04-07 18:50:25 +0000

    Implement a get_json method from utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,8 @@
 """
 from parameterized import parameterized
 import unittest
-from utils import access_nested_map
+from unittest.mock import Mock, patch
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +27,25 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as msg:
             access_nested_map(np, path)
         self.assertEqual(str(msg.exception), expected_msg)
+
+
+class TestGetJson(unittest.TestCase):
+    """ A class to implement the get_json method for mocking """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ Mock the json method of the response object """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
